a4f72fb452f1d084588ef7a0687ed0c56dd2c6e8
Mohammed Manar Yazji <yazjimanar92@gmail.com>
2021-04-23 22:52:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-let  companyName = 'Scuber';
-let  mostProfitableNeighborhood = 'Chelsea';
-let  companyCeo = 'Susan Smith';
+const  companyName = 'Scuber';
+const  mostProfitableNeighborhood = 'Chelsea';
+const  companyCeo = 'Susan Smith';
